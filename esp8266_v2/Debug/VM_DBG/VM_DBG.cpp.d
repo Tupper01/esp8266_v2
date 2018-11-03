74a3084a63cfd3314c973c10b9c7605fbccf2836
Tupper01 <43781867+Tupper01@users.noreply.github.com>
2018-11-03 09:23:41 +0100

    Initial commit

---
+++ esp8266_v2/Debug/VM_DBG/VM_DBG.cpp.d
@@ -0,0 +1,32 @@
+C:\Users\r_tup\AppData\Local\Temp\VMBuilds\esp8266_v2\esp8266_huzzah\Debug\VM_DBG\VM_DBG.cpp.o: \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2017\community\common7\ide\extensions\ktpce3c3.a4z\Micro\ Platforms\default\debuggers\VM_DBG\VM_DBG.cpp \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2017\community\common7\ide\extensions\ktpce3c3.a4z\Micro\ Platforms\default\debuggers\VM_DBG\VM_DBG.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Arduino.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/stdlib_noniso.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/binary.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/esp8266_peri.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0/tools/sdk/include/c_types.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/twi.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Arduino.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/core_esp8266_features.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/pgmspace.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0/tools/sdk/include/ets_sys.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0/tools/sdk/include/c_types.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0/tools/sdk/include/eagle_soc.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0/tools/sdk/include/osapi.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/user_config.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/WCharacter.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/WString.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/pgmspace.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/HardwareSerial.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Stream.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Print.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Printable.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/uart.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Esp.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/Updater.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/flash_utils.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/MD5Builder.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/md5.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\cores\esp8266/debug.h \
+ C:\Users\r_tup\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.1.0\variants\adafruit/pins_arduino.h
---
+++ esp8266_v2/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\r_tup\AppData\Local\Temp\VMBuilds\esp8266_v2\esp8266_huzzah\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2017\community\common7\ide\extensions\ktpce3c3.a4z\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
